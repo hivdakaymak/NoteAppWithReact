6a45c7a2757c38c58a55b042bb90e5a2be12029b
miraykaymak <miirayazz00@gmail.com>
2022-08-17 20:49:31 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
-import TodoForm from "./compenents/TodoForm";
-import TodoList from "./compenents/TodoList";
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="todo-app">
+    <div className='todo-app'>
       <TodoList />
     </div>
   );
